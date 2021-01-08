9122b5555b80b8007f6a7f592f6d9e5da8705ecc
Gideon Akunana <gideonakunana@gmail.com>
2021-01-08 23:28:58 +0100

    added class of register

---
+++ register.php
@@ -0,0 +1,15 @@
+<?php
+   // class registrar
+    // should have properties which would be a multidimensional array.
+    // then methods
+    // Echo on the html to output the table contents of the registrar
+
+    
+    class Register {
+        public $semester_result = [
+            "course" => ['MEC 301', 'MTH 311', 'EED 414', 'GNS 401', 'EEI 434'],
+            "grade" => ['A', 'B', 'A', 'B', 'D']
+        ];
+        
+    }
+?>
