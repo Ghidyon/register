264003fc4923d8ca1accd7e4ca8f2985a46cdc9d
Gideon Akunana <gideonakunana@gmail.com>
2021-01-08 23:47:58 +0100

    added foreach loop

--- register.php
+++ register.php
@@ -12,4 +12,24 @@
         ];
         
     }
+
+    $semester_result = [
+        "course" => ['MEC 301', 'MTH 311', 'EED 414', 'GNS 401', 'EEI 434'],
+        "grade" => ['A', 'B', 'A', 'B', 'D']
+    ];
+
+    $serial_no = 1;    
+    foreach ($semester_result as $key => $value) {
+        echo($key);
+        echo "<br>";
+        for ($i=0; $i < count($value); $i++) { 
+            # code...
+            echo $serial_no;
+            echo "<br>";
+            echo $value[$i];
+            echo "<br>";
+            $serial_no++;
+        }
+        echo "<br>";
+    }
 ?>
